31b9c93f4b4bc88a98b46cdf134bf83bcd79c526
Eli <elichenkova@gmail.com>
2013-07-13 00:37:43 +0200

    bug fix.

--- application/views/admins/create.php
+++ application/views/admins/create.php
@@ -1,6 +1,6 @@
-<?php echo form_open(base_url( isset($admin) ? 'admins/edit/'.$admin->getId() : 'admins/create/')) ?>
+<?php echo form_open(base_url( !empty($admin) ? 'admins/edit/'.$admin->getId() : 'admins/create/')) ?>
 	<label for="name">User name: </label> 
-	<input type="input" name="name" value="<?= set_value('name', (isset($admin) ? $admin->getUsername() : ''))?>" size="30" />
+	<input type="input" name="name" value="<?= set_value('name', (!empty($admin) ? $admin->getUsername() : ''))?>" size="30" />
 	<?= form_error('name','<span class="error">', '</span>'); ?>
 	<br />
 	<label for="password">Password: </label> 
@@ -8,7 +8,7 @@
 	<?= form_error('password','<span class="error">', '</span>'); ?>
 	<br />
 	<label for="email">E-mail: </label> 
-	<input type="input" name="email" value="<?= set_value('email', (isset($admin) ? $admin->getEmail() : '')) ?>" size="30" />
+	<input type="input" name="email" value="<?= set_value('email', (!empty($admin) ? $admin->getEmail() : '')) ?>" size="30" />
 	<?= form_error('email','<span class="error">', '</span>'); ?>
 	<br />
 	<input type="submit" name="submit" value="Save" /> 
